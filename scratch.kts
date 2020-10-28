799cbd98e9e7635e78bad0a3c7d8d94df6534f44
Programador Novato <info@programadornovato.com>
2020-10-27 20:31:03 -0600

    Excepciones en kotlin (Curso de Kotlin desde cero [19])
    Video:
    https://youtu.be/KoOU0ACRXF8

--- scratch.kts
+++ scratch.kts
@@ -1,17 +1,28 @@
+import java.lang.Exception
+
 println("Hola Humano, por favor ingresa una opcion:")
 println("1.- Para acceder")
 println("2.- Para configuracion")
 println("3.- Para ayuda")
 println("4,5,6,7,8,9.- Para salir")
-var opcion= readLine()!!.toInt()
-when (opcion){
-    1-> {
-        println("Seleccionaste acceder")
-        println("Ingresa tu usuario")
-        println("Ingresa tu contraseña")
+try {
+    menu()
+}catch (e:Exception){
+    println("Tienes un error ${e}")
+}finally {
+    println("Finalizo la tarea")
+}
+fun menu(){
+    var opcion= readLine()!!.toInt()
+    when (opcion){
+        1-> {
+            println("Seleccionaste acceder")
+            println("Ingresa tu usuario")
+            println("Ingresa tu contraseña")
+        }
+        2-> println("Seleccionaste configuracion")
+        3-> println("Seleccionaste ayuda")
+        in(4..9)-> println("Adios humano te voy a extrañar")
+        else-> println("Humano estupido la opcion ${opcion} no esta en la put** lista")
     }
-    2-> println("Seleccionaste configuracion")
-    3-> println("Seleccionaste ayuda")
-    in(4..9)-> println("Adios humano te voy a extrañar")
-    else-> println("Humano estupido la opcion ${opcion} no esta en la put** lista")
 }
