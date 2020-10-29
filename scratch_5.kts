2ebb981b6d30d6e4c4e1638b530bf54a3397b959
Programador Novato <info@programadornovato.com>
2020-10-28 20:11:30 -0600

    Operadores de asignación en Kotlin (Curso de Kotlin desde cero [22])
    Video:
    https://youtu.be/ChYExWKZhP4

---
+++ scratch_5.kts
@@ -0,0 +1,20 @@
+var a=6.0
+var b=5.0
+println("a=${a} b=${b}")
+/*
+a-=b
+println("a=${a} b=${b}")
+//a=a*b
+a*=b
+println("a=${a} b=${b}")
+//a=a/b
+a/=b
+println("a=${a} b=${b}")
+*/
+//a=a%b
+a%=b
+println("a=${a} b=${b}")
+
+
+
+
