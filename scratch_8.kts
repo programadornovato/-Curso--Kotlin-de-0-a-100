825f49f2b657a02a620344a6d14640aec182e04b
Programador Novato <info@programadornovato.com>
2020-10-30 05:58:14 -0600

    Ejercicio.- Juego de azar con dados (Curso de Kotlin desde cero [29])
    Video:
    https://youtu.be/70G1srBM9fw

---
+++ scratch_8.kts
@@ -0,0 +1,22 @@
+var d1=(1..6).random()
+var d2=(1..6).random()
+var d3=(1..6).random()
+println("Dado 1=${d1}")
+println("Dado 2=${d2}")
+println("Dado 3=${d3}")
+
+if(d1==6 && d2==6 && d3==6){
+    println("Excelnte")
+}else{
+    if( (d1==6 && d2==6) || (d1==6 && d3==6) || (d2==6 && d3==6) ){
+        println("Muy bien")
+    }else{
+        if(d1==6 || d2==6 || d3==6){
+            println("Regular")
+        }else{
+            if(d1!=6 && d2!=6 && d3!=6){
+                println("Pesimo")
+            }
+        }
+    }
+}
