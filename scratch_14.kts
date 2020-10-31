6b8283ef54210257787ac85e53bbc36fbdab53e9
Programador Novato <info@programadornovato.com>
2020-10-30 23:52:35 -0600

    Ciclo do while (Curso de Kotlin desde cero [31])
    Video:
    https://youtu.be/_T3Rowr5e0c

---
+++ scratch_14.kts
@@ -0,0 +1,22 @@
+/*
+var i=11
+do {
+    println("i=${i}")
+    i++
+}while (i<=10)
+
+var i=-10
+do {
+    println("i=${i}")
+    i--
+}while (i>=1)
+*/
+var aleatorio=(1..6).random()
+println("aleatorio=${aleatorio}")
+var i=10
+do{
+    println("i=${i}")
+    i++
+}while (i<=aleatorio)
+
+
