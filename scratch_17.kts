60c6eb0a294c4c4ab4f705ace229e894bcd6dbaa
Programador Novato <info@programadornovato.com>
2020-11-03 20:14:30 -0600

    Arreglos (Curso de Kotlin desde cero [35])
    Video:
    https://youtu.be/cRruV1JC2q8

---
+++ scratch_17.kts
@@ -0,0 +1,10 @@
+var arregloNumero= arrayOf(5,6,8,7,9,2,3,4)
+//println("Elemento 1=${arregloNumero[5]}")
+//println("Indices=${arregloNumero.indices}")
+for(i in arregloNumero.indices){
+    println("Elemento ${i}=${arregloNumero[i]}")
+}
+
+
+
+
