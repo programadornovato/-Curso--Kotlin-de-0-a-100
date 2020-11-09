6e69326ffde83f86f68c631ccb65114cc1e44992
Programador Novato <info@programadornovato.com>
2020-11-09 02:01:19 -0600

    Métodos y Atributos Públicos y Privados en Kotlin (Curso de Kotlin desde cero [55])
    Video:
    https://youtu.be/zeHg5mgnFAA

--- scratch_35.kts
+++ scratch_35.kts
@@ -1,29 +1,37 @@
 class auto{
-    var color=""
-    var modelo=0
-    var marca=""
-    fun enciende(){
+    private var color=""
+    private var modelo=0
+    private var marca=""
+    private var acceso=false
+    public fun meterLlave(llave:String){
+        if(llave=="123456"){
+            acceso=true
+            println("Tienes acceso al auto")
+        }else{
+            acceso=false
+            println("No tienes acceso al auto (sonar alarma)")
+        }
+    }
+    public fun mando(accion:String){
+        if(acceso==true){
+            if(accion=="encender"){
+                enciende()
+            }
+        }else{
+            println("No tienes acceso al auto (sonar alarma)")
+        }
+    }
+    private fun enciende(){
         println("Enciende el auto run run")
     }
-    fun acelera(){
+    private fun acelera(){
         println("El auto acelero 10Km/h mas")
     }
-    fun frena(){
+    private fun frena(){
         println("El auto freno a 10Km/h menos")
     }
 }
-var vocho1=auto()
-vocho1.color="Verde"
-vocho1.modelo=1980
-vocho1.marca="Vocho"
-println(vocho1.color)
-println(vocho1.modelo)
-println(vocho1.marca)
-vocho1.enciende()
-vocho1.acelera()
-vocho1.frena()
 
-var vocho2=auto()
-vocho2.color="Azul"
-vocho2.modelo=1981
-vocho2.marca="Vocho"
+var vocho1=auto()
+vocho1.meterLlave("123456")
+vocho1.mando("encender")
